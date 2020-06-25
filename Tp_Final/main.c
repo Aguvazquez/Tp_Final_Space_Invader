44bcfec13da72315488924d2f77adfec6bed39d8
aguvazquez <aguvazquez@opc-l-wrk-0004.itbadc.itba.edu.ar>
2020-06-25 13:11:12 -0300

    main, espero que esto se este subiendo.

---
+++ Tp_Final/main.c
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: aguvazquez
+ *
+ * Created on 25 de junio de 2020, 12:31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <allegro5/allegro.h>  
+#include <allegro5/allegro_color.h> 
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h> 
+#include <allegro5/allegro_ttf.h> 
+#include <allegro5/allegro_audio.h> 
+#include <allegro5/allegro_acodec.h> 
+/*
+ * 
+ */
+int main(void) {
+    ALLEGRO_DISPLAY * display = NULL;
+    ALLEGRO_EVENT_QUEUE * event_queue=NULL;
+    ALLEGRO_TIMER * timer=NULL;
+    ALLEGRO_FONT *font=NULL;
+    ALLEGRO_SAMPLE * sample_disp=NULL;
+    ALLEGRO_SAMPLE * sample_song1=NULL;
+    
+    return (EXIT_SUCCESS);
+}
+
