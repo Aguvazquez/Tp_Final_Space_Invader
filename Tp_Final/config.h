03909025937b0464c7ab5ddd76b419c866f50cbb
aguvazquez <aguvazquez@opc-l-wrk-0004.itbadc.itba.edu.ar>
2020-06-25 13:09:36 -0300

    Header de defines , por favor incluir en todos los .c

---
+++ Tp_Final/config.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   config.h
+ * Author: aguvazquez
+ *
+ * Created on 25 de junio de 2020, 12:42
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SCREEN_W = 800
+#define SCREEN_H = 640
+#define SAMPLES = 1
+#endif//CONFIG_H
+
+
+
+
