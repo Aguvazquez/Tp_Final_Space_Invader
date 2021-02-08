b0c25efdf87bd62d5950624abf9b9edd3cbcf079
frmendizabal <frmendizabal@opc-l-wrk-0007.itbadc.itba.edu.ar>
2021-02-08 13:59:22 -0300

    mas prolijo, ahora imprime error cuando no se puede sobreescribir top score

--- Tp_Final/back_end.c
+++ Tp_Final/back_end.c
@@ -33,7 +33,33 @@ static void reorder_top_score(void);
 static uint32_t string_to_int(char str[STR_LONG]);
 
 /*******************************************************************************/
+
 /*************************** Global functions **********************************/
+
+uint8_t create_Top_Score(void) {
+
+    if (!fopen(".Top_Score.txt", "r")) { //en caso de no encontrar el archivo del top score
+        int i, j;
+        FILE* fp;
+        fp = fopen(".Top_Score.txt", "w"); //crea el top score vacío, con el formato apropiado
+        if (!fp) {
+            fprintf(stderr, "Hubo un error en la creación de Top_Score.\n");
+            return EXIT_FAILURE;
+        }
+        for (i = 0; i < TOP_PLAYERS; i++) {
+            for (j = 0; j < STR_LONG - 1; j++) { //SCORE
+                fputc('0', fp);
+            }
+            fputc('\n', fp);
+            for (j = 0; j < STR_LONG - 1; j++) { //NAME
+                fputc(' ', fp);
+            }
+            fputc('\n', fp);
+        }
+        fclose(fp);
+    }
+    return EXIT_SUCCESS;
+}
 
 int8_t get_top_score(uint32_t score) { //devuelve la posicion del jugador si esta en el top 5, sino un 0
 
@@ -62,6 +88,61 @@ int8_t get_top_score(uint32_t score) { //devuelve la posicion del jugador si est
 
     fclose(fp);
     return EXIT_SUCCESS; //si sale del ciclo significa que no entró al top
+}
+
+void put_on_top_score(uint32_t score, char *name_str) {
+
+    FILE* fp;
+    uint32_t j, aux = 0;
+    int8_t i;
+
+    char score_str[STR_LONG] = {'0', '0', '0', '0', '0', '\0'};
+    for (i = STR_LONG - 2, j = 1; i >= 0; i--, j *= 10) { //ya que el último dígito está en penúltima posición
+        aux = score / j; //algoritmo int -> char
+        score_str[i] = (char) ((aux % 10) + ASCII);
+    }
+
+    fp = fopen(".Top_Score.txt", "a"); //escribo al final del archivo.
+    if(!fp) {
+        fprintf(stderr, "Hubo un error al sobreescribir el top score.\n");
+    }
+    else {
+        fputs(score_str, fp); //SCORE
+        fputc('\n', fp);
+        fputs(name_str, fp); //NAME
+        fputc('\n', fp);
+        fclose(fp);
+        reorder_top_score();
+    }
+}
+
+uint8_t create_difficulty(void){     //agregar devolucion de error   
+    
+    FILE* fp;     
+    if(!fopen(".Difficulty.txt", "r")){         
+        fp=fopen(".Difficulty.txt", "w");         
+        if(!fp){
+            return EXIT_FAILURE;
+        }
+        switch_difficulty(2);   //Setea la dificultad en normal, en caso de no encontrar el archivo o que 
+                                //el contenido se haya modificado fraudulentamente. 
+        fclose(fp);
+    }
+    return EXIT_SUCCESS;
+}
+
+int8_t read_difficulty(void){
+    
+    FILE* fp = fopen(".Difficulty.txt", "r");
+    if(!fp){
+        fprintf(stderr, "Hubo un error al leer la dificultad.\n");
+        return FATAL_ERROR;
+    }
+    int8_t difficulty;
+    difficulty=(fgetc(fp)-ASCII)*10;    //Convierto en la decena
+    difficulty+=(fgetc(fp)-ASCII);      //Le sumo la unidad
+    fclose(fp);
+    return difficulty;
 }
 
 int8_t switch_difficulty(uint8_t option){
@@ -96,84 +177,6 @@ int8_t switch_difficulty(uint8_t option){
     fclose(fp);
     return aux;    
 }
-
-void put_on_top_score(uint32_t score, char *name_str) {
-
-    FILE* fp;
-    uint32_t j, aux = 0;
-    int8_t i;
-
-    char score_str[STR_LONG] = {'0', '0', '0', '0', '0', '\0'};
-    for (i = STR_LONG - 2, j = 1; i >= 0; i--, j *= 10) { //ya que el último dígito está en penúltima posición
-        aux = score / j; //algoritmo int -> char
-        score_str[i] = (char) ((aux % 10) + ASCII);
-    }
-
-    fp = fopen(".Top_Score.txt", "a"); //escribo al final del archivo.
-    fputs(score_str, fp); //SCORE
-    fputc('\n', fp);
-    fputs(name_str, fp); //NAME
-    fputc('\n', fp);
-    fclose(fp);
-
-    reorder_top_score();
-}
-
-int8_t read_difficulty(void){
-    
-    FILE* fp = fopen(".Difficulty.txt", "r");
-    if(!fp){
-        fprintf(stderr, "Hubo un error al leer la dificultad.\n");
-        return FATAL_ERROR;
-    }
-    int8_t difficulty;
-    difficulty=(fgetc(fp)-ASCII)*10;    //Convierto en la decena
-    difficulty+=(fgetc(fp)-ASCII);      //Le sumo la unidad
-    fclose(fp);
-    return difficulty;
-}
-
-int8_t create_difficulty(void){     //agregar devolucion de error   
-    
-    FILE* fp;     
-    if(!fopen(".Difficulty.txt", "r")){         
-        fp=fopen(".Difficulty.txt", "w");         
-        if(!fp){
-            return EXIT_FAILURE;
-        }
-        switch_difficulty(2);   //Setea la dificultad en normal, en caso de no encontrar el archivo o que 
-                                //el contenido se haya modificado fraudulentamente. 
-        fclose(fp);
-    }
-    return EXIT_SUCCESS;
-}
-
-uint8_t create_Top_Score(void) {
-
-    if (!fopen(".Top_Score.txt", "r")) { //en caso de no encontrar el archivo del top score
-        int i, j;
-        FILE* fp;
-        fp = fopen(".Top_Score.txt", "w"); //crea el top score vacío, con el formato apropiado
-        if (!fp) {
-            fprintf(stderr, "Hubo un error en la creación de Top_Score.\n");
-            return EXIT_FAILURE;
-        }
-        for (i = 0; i < TOP_PLAYERS; i++) {
-            for (j = 0; j < STR_LONG - 1; j++) { //SCORE
-                fputc('0', fp);
-            }
-            fputc('\n', fp);
-            for (j = 0; j < STR_LONG - 1; j++) { //NAME
-                fputc(' ', fp);
-            }
-            fputc('\n', fp);
-        }
-        fclose(fp);
-    }
-    return EXIT_SUCCESS;
-}
-
-
 
 /*******************************************************************************/
 
@@ -217,13 +220,13 @@ static void reorder_top_score(void) {
         ++puser;
         --all_names;
     }
-    for (i = 0; i < TOP_PLAYERS + 1; i++) { //ya que tendremos un jugador de más al ejecutarse la función
+    for (i = 0; i < TOP_PLAYERS + 1; i++) {     //ya que tendremos un jugador de más al ejecutarse la función
         users[i].score_num = string_to_int(users[i].score);
     }
     for (i = 0; i < TOP_PLAYERS + 1; i++) {
         for (j = 1 + i; j < TOP_PLAYERS + 1; j++) {
             if ((users[i].score_num) < (users[j].score_num)) { //compara al nuevo jugador con los demás
-                user_aux = users[i]; //para llevarlo a su posición correspondiente
+                user_aux = users[i];                           //para llevarlo a su posición correspondiente
                 users[i] = users[j];
                 users[j] = user_aux;
             }
--- Tp_Final/back_end.h
+++ Tp_Final/back_end.h
@@ -1,12 +1,12 @@
 /*******************************************************************************
- *                                Top Score                                    *
+ *                                Back End                                     *
  *                                                                             *
  * Desarrolladores: Mendizabal Francisco(61454), Vazquez Agustin(61420)        *
  * Todos los derechos reservados.                                              *
  *******************************************************************************/
 
-#ifndef TOP_SCORE_H
-#define TOP_SCORE_H
+#ifndef BACK_END_H
+#define BACK_END_H
 
 /*
  * @Brief crea el top score con el formato apropiado, en caso que no se encuentre el archivo. 
@@ -35,14 +35,20 @@ int8_t get_top_score(uint32_t score);
 void put_on_top_score(uint32_t score, char *str);
 
 /*
+ * @Brief crea el archivo de dificultad en dificultad "normal", en caso que no se encuentre el archivo. 
+ * @Return  EXIT_FAILURE si no encontró ni pudo crear el archivo.
+ *          EXIT_SUCCESS en caso contrario.
+ */
+
+uint8_t create_difficulty(void);
+
+/*
  * @Brief lee la dificultad, guardada en el archivo oculto. 
  * @Return  el valor leido.
  *          FATAL_ERROR si hay un error.
  */
 
 int8_t read_difficulty(void);
-
-int8_t create_difficulty(void);
 
 /*
  * @Brief Permite cambiar la dificultad del juego en el archivo. 
